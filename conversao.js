ff51eab70383e7eaca34fe619a519834563e2c94
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-08-25 23:03:45 -0300

    Conversão implicita e explicita

---
+++ conversao.js
@@ -0,0 +1,17 @@
+ //tipo de dado
+ //booleanos
+
+ //conversao implicita
+ const numero = 450;
+ const numeroString = "450"
+
+ console.log(numero === numeroString);
+ console.log(numero == numeroString);
+
+// Number()
+// String()
+ console.log(numero + Number(numeroString));
+ console.log(String(numero) + numeroString);
+
+
+ //conversao explicita
