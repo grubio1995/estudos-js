9b466157fca117778d5344a245a771167ef3664d
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-10-31 20:36:59 -0300

    exemplo utilizando template-string

---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = 'Gabriel';
+const idade =  2022 - 1995;
+const cidadeDeNascimento = 'Piraju';
+
+const apresentacao = "meu nome é " + nome + " , minha idade é " + idade + 
+" e nasci na cidade de " + cidadeDeNascimento;
+
+console.log(apresentacao);
