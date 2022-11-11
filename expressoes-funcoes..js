8527d6dcc26b1a2802df4fea4ef10e8d49730bc2
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-11-10 22:09:46 -0300

    expressoes-funcoes

---
+++ expressoes-funcoes..js
@@ -0,0 +1,13 @@
+//expressão da função
+
+
+// diferença principal: HOISTING
+// funções e var são "listadas" no topo do arquivo.
+
+console.log(apresentar());
+function apresentar() {
+  return "olá";
+}
+
+console.log(soma(1,2))
+const soma = function(num1, num2){return num1 + num2}
