d079103555ccc0a1c9b5d7d4f0fcdec4cd6ca907
grubio1995 <gabriel_pinhatarubio@hotmail.com>
2023-04-30 20:54:22 -0300

    For clássico

--- js-arrays/procurando-aluno.js
+++ js-arrays/procurando-aluno.js
@@ -3,12 +3,8 @@ const medias = [10, 8, 7.5, 9];
 const listaDeAlunosEMedias = [alunos, medias];
 
 
-
 function exibeNomeNota(aluno){
     if(listaDeAlunosEMedias[0].includes(aluno)){
-        // const alunos = listaDeAlunosEMedias[0];
-        // const medias = listaDeAlunosEMedias[1];
-
         const[alunos, medias] = listaDeAlunosEMedias;
 
         const indice = alunos.indexOf(aluno);
