8cd55cb7543e21e4f284208485e687bce2008183
grubio1995 <gabriel_pinhatarubio@hotmail.com>
2023-04-30 19:37:56 -0300

    Removendo elementos do array

--- js-arrays/media.js
+++ js-arrays/media.js
@@ -5,6 +5,6 @@
 
 const notas = [10.6, 6.8, 8,  7.5];
 
-const media = (notas[0] + notas[1], notas[2], notas[3]) / notas.length;
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
