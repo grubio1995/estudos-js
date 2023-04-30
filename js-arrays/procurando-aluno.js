bc70087aa8249b2e96a87bdd75f90d85e6cabe0e
grubio1995 <gabriel_pinhatarubio@hotmail.com>
2023-04-30 20:46:48 -0300

    Procurando na lista

---
+++ js-arrays/procurando-aluno.js
@@ -0,0 +1,20 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+const listaDeAlunosEMedias = [alunos, medias];
+
+
+
+function exibeNomeNota(aluno){
+    if(listaDeAlunosEMedias[0].includes(aluno)){
+
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+
+        console.log(`${aluno} tem a média ${mediaDoAluno}`);
+    } else{
+        console.log("Aluno não encontrado!");
+    }
+}
+
+
+exibeNomeNota("João");
